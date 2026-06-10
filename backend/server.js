f7422f1f6cc0509c370e14f527350b52c68749c5
santosh-sirpi <santosh.sirpi@gmail.com>
2026-06-10 11:54:46 +0530

    fix: revert express 5 catch-all route syntax to *any

--- backend/server.js
+++ backend/server.js
@@ -1392,7 +1392,7 @@ io.on('connection', (socket) => {
 
 // Catch-all route to serve the frontend index.html for any unknown routes
 // This MUST be the last route in your app
-app.get('*', (req, res) => {
+app.get('*any', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
